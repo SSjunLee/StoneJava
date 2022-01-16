390516d791908afc100c1258b2cc27d28ee9a0d8
ljn <91297861+SSjunLee@users.noreply.github.com>
2022-01-16 16:20:14 +0800

    定义类

---
+++ src/main/java/com/ljn/stone/StoneClassInfo.java
@@ -0,0 +1,36 @@
+package com.ljn.stone;
+
+import com.ljn.stone.ast.impl.ClassBody;
+import com.ljn.stone.ast.impl.ClassStmt;
+import com.ljn.stone.env.Env;
+import com.ljn.stone.exception.StoneException;
+
+public class StoneClassInfo {
+    private ClassStmt classDef;
+    private StoneClassInfo parent;
+    private Env env;
+
+    public StoneClassInfo(ClassStmt classDef, Env env) {
+        this.classDef = classDef;
+        Object parent = env.get(classDef.superClass());
+        if(parent!=null ){
+            if(parent instanceof StoneClassInfo)
+            this.parent = (StoneClassInfo) parent;
+            else
+                throw new StoneException("unknown super class "+classDef.superClass(),classDef);
+        }else{
+            this.parent = null;
+        }
+
+        this.env = env;
+    }
+    public String name(){return classDef.name();}
+    public ClassBody body(){return classDef.body();}
+    public StoneClassInfo superClass() {return parent;}
+    public Env env(){return env;}
+
+    @Override
+    public String toString() {
+        return "<class "+name()+" "+parent+" "+body()+" >";
+    }
+}
--- src/main/java/com/ljn/stone/ast/impl/ClassStmt.java
+++ src/main/java/com/ljn/stone/ast/impl/ClassStmt.java
@@ -1,12 +1,37 @@
 package com.ljn.stone.ast.impl;
 
+import com.ljn.stone.StoneClassInfo;
+import com.ljn.stone.Token;
+import com.ljn.stone.ast.ASTLeaf;
 import com.ljn.stone.ast.ASTList;
 import com.ljn.stone.ast.ASTree;
+import com.ljn.stone.env.Env;
 
 import java.util.List;
 
 public class ClassStmt extends ASTList {
     public ClassStmt(List<ASTree> list) {
         super(list);
+    }
+    public String name(){return ((ASTLeaf) child(0)).token().getText();}
+    public String superClass(){
+        if(numChildren()<3)return null;
+        return  ((ASTLeaf) child(1)).token().getText();
+    }
+    public ClassBody body(){
+        return (ClassBody) child(numChildren()-1);
+    }
+
+    @Override
+    public String toString() {
+        String father = superClass();
+        return "(class "+father+" "+hashCode()+" )";
+    }
+
+    @Override
+    public Object eval(Env env) {
+        StoneClassInfo classInfo = new StoneClassInfo(this,env);
+        env.put(classInfo.name(),classInfo);
+        return name();
     }
 }

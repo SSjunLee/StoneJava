fbb9402e534fffa316e99017ce10182a9067d834
ljn <91297861+SSjunLee@users.noreply.github.com>
2022-01-16 16:43:01 +0800

    重构ParseExpr.eval

--- src/main/java/com/ljn/stone/ast/impl/ClassBody.java
+++ src/main/java/com/ljn/stone/ast/impl/ClassBody.java
@@ -2,11 +2,20 @@ package com.ljn.stone.ast.impl;
 
 import com.ljn.stone.ast.ASTList;
 import com.ljn.stone.ast.ASTree;
+import com.ljn.stone.env.Env;
 
 import java.util.List;
 
 public class ClassBody extends ASTList {
     public ClassBody(List<ASTree> list) {
         super(list);
+    }
+
+    @Override
+    public Object eval(Env env) {
+        for(ASTree stmt:this){
+            stmt.eval(env);
+        }
+        return null;
     }
 }
--- src/main/java/com/ljn/stone/ast/impl/PrimaryExpr.java
+++ src/main/java/com/ljn/stone/ast/impl/PrimaryExpr.java
@@ -15,13 +15,29 @@ public class PrimaryExpr extends ASTList {
         return c.size()==1?c.get(0):new PrimaryExpr(c);
     }
     public ASTree operand(){return child(0);}
+
+    public boolean hasPostfix(int nest){return numChildren() - nest>1;}
+    public Postfix getPostfix(int nest){return (Postfix) child(numChildren()-nest-1);}
+
+    public Object evalSubExpr(int nest,Env env){
+        if(hasPostfix(nest)){
+            Object target = evalSubExpr(nest+1,env);
+            return getPostfix(nest).eval(env,target);
+        }else{
+            return operand().eval(env);
+        }
+    }
+
+
     @Override
     public Object eval(Env env) {
+        /*
         Object res = operand().eval(env);
         for (int i = 1; i < numChildren(); i++) {
             Postfix postfix = (Postfix) child(i);
             res= postfix.eval(env,res);
         }
-        return res;
+        return res;*/
+        return evalSubExpr(0,env);
     }
 }

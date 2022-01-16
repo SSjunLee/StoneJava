5fbc6512d24cc94685430e01fe1bd41ed12f51e5
ljn <91297861+SSjunLee@users.noreply.github.com>
2022-01-16 17:36:59 +0800

    实现对象

--- src/main/java/com/ljn/stone/StoneClassInfo.java
+++ src/main/java/com/ljn/stone/StoneClassInfo.java
@@ -6,31 +6,46 @@ import com.ljn.stone.env.Env;
 import com.ljn.stone.exception.StoneException;
 
 public class StoneClassInfo {
-    private ClassStmt classDef;
-    private StoneClassInfo parent;
+    private final ClassStmt classDef;
+    private final StoneClassInfo parent;
     private Env env;
 
     public StoneClassInfo(ClassStmt classDef, Env env) {
         this.classDef = classDef;
-        Object parent = env.get(classDef.superClass());
-        if(parent!=null ){
-            if(parent instanceof StoneClassInfo)
-            this.parent = (StoneClassInfo) parent;
-            else
-                throw new StoneException("unknown super class "+classDef.superClass(),classDef);
-        }else{
+        if (classDef.superClass() != null) {
+            Object parent = env.get(classDef.superClass());
+            if (parent != null) {
+                if (parent instanceof StoneClassInfo)
+                    this.parent = (StoneClassInfo) parent;
+                else
+                    throw new StoneException("unknown super class " + classDef.superClass(), classDef);
+            } else {
+                throw new StoneException("unknown super class " + classDef.superClass(), classDef);
+            }
+        } else {
             this.parent = null;
         }
-
         this.env = env;
     }
-    public String name(){return classDef.name();}
-    public ClassBody body(){return classDef.body();}
-    public StoneClassInfo superClass() {return parent;}
-    public Env env(){return env;}
+
+    public String name() {
+        return classDef.name();
+    }
+
+    public ClassBody body() {
+        return classDef.body();
+    }
+
+    public StoneClassInfo superClass() {
+        return parent;
+    }
+
+    public Env env() {
+        return env;
+    }
 
     @Override
     public String toString() {
-        return "<class "+name()+" "+parent+" "+body()+" >";
+        return "<class " + name() + " " + parent + " " + body() + " >";
     }
 }
